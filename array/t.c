7f11d6c49e9915e8e23f4d132401e74e65540788
lanian <uamyd5279@gmail.com>
2013-08-10 14:01:18 +0900

    mod

--- array/t.c
+++ array/t.c
@@ -1,40 +1,13 @@
 #include <stdio.h>
 
-//int sum(int from, int to, int arr[])
-int sum(int from, int to, int *arr)
+int main(void)
 {
-	int i, dRet;
-	for( dRet = 0, i = from; i < to; i++ ){
-		//dRet += arr[i];
-		dRet += *(arr+i);
-	}
-	return dRet;
-}
+	int *n;
+	int r;
+	n[0] = 1;
+	n[1] = 2;
 
-int main(int ac, int **av)
-{
-	int from, to;
-	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
-
-	if( ac != 3 ){
-		printf("param needs\n");
-		return -1;
-	}
-	
-	from = atoi(av[1]);
-	to   = atoi(av[2]);
-
-	if( from < 0 || from > 10 ){
-		printf("from value is must larger than 0, smller than 10, from=%d\n", from);
-		return -2;
-	}
-
-	if( to < 0 || to > 10 ){
-		printf("to value is must larger than 0, smller than 10, to=%d\n", to);
-		return -3;
-	}
-
-	printf("from=%d, to=%d, sum=%d\n", from, to, sum(from,to,&arr[0]));
-	return 0;
-
+	printf("n[0]=%d\n", n[0]);
+	printf("n[1]=%d\n", n[1]);
+	return;
 }
--- bit/t.c
+++ bit/t.c
@@ -1,1 +1,27 @@
-bit1.c
+#include <stdio.h>
+
+#define AIS_NORMAL(a,b) ((a >> (8-(b*2))) & 0x01)
+#define AIS_ALARM(a,b)  ((a >> (8-(b*2))) & 0x02)
+
+#define IS_ACTIVE(a) (a & 0x03)
+
+int main()
+{
+	char a = 0x40;
+	char b = 0x30;
+	char c = 0x0c;
+	char d = 0x03;
+
+	if( (a >> 6) & 0x01 ) printf("a normal\n");
+	if( (a >> 6) & 0x02 ) printf("a alarm\n");
+	if( IS_ACTIVE(0x02) ) printf("S normal1\n");
+	if( IS_ACTIVE(0x01) ) printf("S normal2\n");
+	if( (a >> 6) & 0x02 ) printf("a alarm\n");
+	if( (b >> 4) & 0x01 ) printf("b normal\n");
+	if( (b >> 4) & 0x02 ) printf("b alarm\n");
+	if( (c >> 2) & 0x01 ) printf("c normal\n");
+	if( (c >> 2) & 0x02 ) printf("c alarm\n");
+	if( (d >> 0) & 0x01 ) printf("d normal\n");
+	if( (d >> 0) & 0x02 ) printf("d alarm\n");
+	return 0;
+}
